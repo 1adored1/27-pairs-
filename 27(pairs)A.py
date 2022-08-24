f25b4054da4aaab31bba2672adc21aa74c5a0f16
adored <perekrutq@gmail.com>
2022-08-24 11:56:36 +0500

    Только для n < 50

---
+++ 27(pairs)A.py
@@ -0,0 +1,13 @@
+f = open('test.txt')
+n = int(f.readline())
+s = [0]
+
+mx = -float('inf')
+for i in range(n):
+    pair = [int(x) for x in f.readline().split()]
+    s = [a + b for a in s for b in pair]
+for x in s:
+    if x % 3 == 0:
+        mx = max(mx, x)
+print(mx)
+
